dd45fa1c67f597f1feed9e7405fcf5feeebc8d1d
mgroesink <mroesink@rocvantwente.nl>
2018-04-05 10:29:33 +0200

    Genre create added

---
+++ MvcMusicStore/Controllers/GenreController.cs
@@ -0,0 +1,35 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class GenreController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        // GET: Genres
+        public ActionResult Index()
+        {
+            return View(context.Genres);
+        }
+
+        // GET: Genres
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Genres
+        [HttpPost]
+        public ActionResult Create(Genre genre)
+        {
+            context.Genres.Add(genre);
+            context.SaveChanges();
+            return View();
+        }
+    }
+}
