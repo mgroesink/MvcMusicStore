2e82c809fc868c87fa68fc28f6788852fd3d5581
mgroesink <mroesink@rocvantwente.nl>
2018-04-06 07:28:52 +0200

    Details genre added

--- MvcMusicStore/Controllers/GenreController.cs
+++ MvcMusicStore/Controllers/GenreController.cs
@@ -32,9 +32,46 @@ namespace MvcMusicStore.Controllers
             return View();
         }
 
+        public ActionResult Details(int id)
+        {
+            Genre genre = context.Genres.FirstOrDefault(
+                g => g.Id == id);
+            return View(genre);
+        }
         public ActionResult Edit(int id)
         {
-            return View();
+            Genre genre = context.Genres.FirstOrDefault(
+                g => g.Id == id);
+            return View(genre);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Genre genre)
+        {
+            int id = genre.Id;
+            Genre oldGenre  = context.Genres.FirstOrDefault(
+                g => g.Id == id);
+            oldGenre.Name = genre.Name;
+            oldGenre.Description = genre.Description;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Genre genre = context.Genres.FirstOrDefault(
+                g => g.Id == id);
+            return View(genre);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Genre genre)
+        {
+            Genre oldGenre = context.Genres.FirstOrDefault(
+                g => g.Id == genre.Id);
+            context.Genres.Remove(oldGenre);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
